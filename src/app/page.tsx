7f57d040bc6e60fbbcf1afe4f2cac87d657c9df4
psycadd <info@wildernessbackpackers.com>
2025-04-01 14:09:20 +0200

    Removed the priority prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -130,7 +130,6 @@ export default function Home() {
                 alt="Fairy Knowe Backpackers Logo"
                 fill
                 className="object-contain"
-                priority
               />
             </div>
           </div>
