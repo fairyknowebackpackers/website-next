015fcb911db1612bcf430fcb73c5b28705780cc9
psycadd <info@wildernessbackpackers.com>
2025-04-01 13:13:27 +0200

    Upgrade Next.js and React to versions 15.2.4 and 19.1.0 respectively; update TypeScript types and configuration references; modify build and manifest files to reflect new structure and added pages.

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Ae+KCZqkqnXq2EDxcY6ONOKRk/WnyrYO345YEP8Tc/0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
