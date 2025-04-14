44239561f80b658334ae2d2b6853367bcb0e9468
psycadd <info@wildernessbackpackers.com>
2025-04-14 17:15:32 +0200

    Adjust home page card layout for improved responsiveness by changing height settings and padding, ensuring better visual consistency across devices.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {pages.map((page) => (
               <Link href={page.link} key={page.title} className="group">
-                <div className="bg-[#E5E7EB] text-[#202635] rounded-lg shadow-lg overflow-hidden transition-all duration-500 ease-in-out transform hover:scale-105 hover:bg-gradient-to-tl hover:from-[#35946E] hover:to-[#094B44] h-[360px] flex flex-col">
+                <div className="bg-[#E5E7EB] text-[#202635] rounded-lg shadow-lg overflow-hidden transition-all duration-500 ease-in-out transform hover:scale-105 hover:bg-gradient-to-tl hover:from-[#35946E] hover:to-[#094B44] h-auto md:h-[360px] flex flex-col">
                   <div className="relative aspect-video md:h-56 flex-shrink-0">
                     <Image
                       src={page.image}
@@ -84,7 +84,7 @@ export default function Home() {
                       quality={75}
                     />
                   </div>
-                  <div className="p-6 flex-1 flex flex-col justify-between">
+                  <div className="p-4 md:p-6 flex-1 flex flex-col justify-between">
                     <h2 className="text-3xl md:text-2xl font-bold mb-1 md:mb-2 text-[#202635] group-hover:text-[#00FF7F] transition-colors duration-500 ease-in-out text-center md:text-left">
                       {page.title}
                     </h2>
