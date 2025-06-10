93d13c3b96166e74418b1c139e638a02425ad2af
getswyft <hello@getswyft.dev>
2025-06-10 15:21:58 +0200

    Fixed the markdown rendering in the accommodation template

--- src/app/accommodation/[slug]/page.tsx
+++ src/app/accommodation/[slug]/page.tsx
@@ -5,10 +5,11 @@ import Hero from '../../../components/Hero';
 import FusionCollection from "fusionable/FusionCollection";
 import { FusionFieldsType, FusionItemType } from "fusionable/FusionItem";
 import Showdown from 'showdown';
+import path from 'path';
 
-
-function getPostBySlug(slug: string):FusionItemType {
-  const collection = new FusionCollection().loadFromDir('content/accommodation');
+function getPostBySlug(slug: string): FusionItemType {
+  const contentPath = path.join(process.cwd(), 'content/accommodation');
+  const collection = new FusionCollection().loadFromDir(contentPath);
   const post = collection.getOneBySlug(slug);
   if (!post) {
     throw new Error('Post not found');
